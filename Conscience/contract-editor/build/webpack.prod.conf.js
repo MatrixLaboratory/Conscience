8bc079b821d76be426800d5adef5cf803114da09
xiaowu <xiaowu>
2019-05-23 01:53:55 +0800

    1
    1

--- Conscience/contract-editor/build/webpack.prod.conf.js
+++ Conscience/contract-editor/build/webpack.prod.conf.js
@@ -81,7 +81,7 @@ const webpackConfig = merge(baseWebpackConfig, {
     new webpack.optimize.ModuleConcatenationPlugin(),
     // split vendor js into its own file
     new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
+      name: ['vendor','common'],
       minChunks (module) {
         // any required modules inside node_modules are extracted to vendor
         return (
