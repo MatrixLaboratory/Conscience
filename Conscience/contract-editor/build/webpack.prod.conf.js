2ecf1ac9b2d03dc8cbc47684dd6d07b3abd05da2
xiaowu <xiaowu>
2019-05-23 02:13:02 +0800

    1
    1

--- Conscience/contract-editor/build/webpack.prod.conf.js
+++ Conscience/contract-editor/build/webpack.prod.conf.js
@@ -100,6 +100,10 @@ const webpackConfig = merge(baseWebpackConfig, {
       name: 'manifest',
       minChunks: Infinity
     }),
+      new webpack.optimize.CommonsChunkPlugin({
+          names: ['vendor', 'mainifest'],
+          minChunks: 1
+      }),
     // This instance extracts shared chunks from code splitted chunks and bundles them
     // in a separate chunk, similar to the vendor chunk
     // see: https://webpack.js.org/plugins/commons-chunk-plugin/#extra-async-commons-chunk
