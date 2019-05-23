bc3dab8213a58c8901afcfa5b6344c9cc9466a56
xiaowu <xiaowu>
2019-05-23 10:30:45 +0800

    webpack

--- Conscience/contract-editor/build/webpack.dev.conf.js
+++ Conscience/contract-editor/build/webpack.dev.conf.js
@@ -42,6 +42,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     publicPath: config.dev.assetsPublicPath,
     proxy: config.dev.proxyTable,
     quiet: true, // necessary for FriendlyErrorsPlugin
+      disableHostCheck:ture,
     watchOptions: {
       poll: config.dev.poll,
     }
