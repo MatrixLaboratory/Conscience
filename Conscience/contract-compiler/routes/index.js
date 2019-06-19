b30630f7de3f9888d2d651d1b62c76f97a9e9ed9
zh <277708272@qq.com>
2019-06-19 14:50:56 +0800

    add pledge & nr nat query

--- Conscience/contract-compiler/routes/index.js
+++ Conscience/contract-compiler/routes/index.js
@@ -4,12 +4,14 @@ const router = express.Router();
 const execLocalBin = require('exec-local-bin');
 const fileSystem = require('file-system');
 const uuidv1 = require('uuid/v1');
+
 const axios = require('axios')
-let nrList = []
-let pledgeList = []
-let totalPage = 0
-let nrNat = 0
-let pledgeNat = 0
+const pledgeAndNrAddress = 'n1EoNsJNXG1tN3z9rvjwPKoBXbJMqAjmESC'
+const voteAddress = 'n1pADU7jnrvpPzcWusGkaizZoWgUywMRGMY'
+const getTxUrl = 'https://explorer-backend.nebulas.io/api/tx/'
+const getEventByHashUrl = 'https://mainnet.nebulas.io/v1/user/getEventsByHash'
+const pledgeAndNrTopic = 'chain.contract.NATToken'
+const voteTopic = 'chain.contract.vote'
 
 const ABI_REGEX = /^Successfully\sgenerated\sabi\sfile\sas:\s(.*)/;
 
@@ -84,81 +86,206 @@ router.post('/solidity/compile', function (req, res, next) {
   res.json(output);
 });
 
-router.get('/nebulas/get/tx', function (req, res) {
-  console.log('req: ', req.query)
-  let address = 'n1EoNsJNXG1tN3z9rvjwPKoBXbJMqAjmESC'
-  let url = 'https://explorer-backend.nebulas.io/api/tx/'
-  nrList = []
-  pledgeList = []
+router.get('/pledge/nat', function (req, res) {
+  let start = req.query.start
+  let end = req.query.end
+  if (start === undefined || end === undefined || start < 0 || end < 0) {
+    res.json('parameter error!')
+  }
+  start = req.query.start - 3600 * 1000 * 24 * 7;
+  end = req.query.end - 3600 * 1000 * 24 * 7
+  let pledgeNat = 0
   let firstParam = {
     params: {
-      a: address,
+      a: pledgeAndNrAddress,
       p: 1
     }
   }
-  axios.get(url, firstParam).then(async response => {
-    let data = response.data
-    totalPage = data.data.totalPage
-    let response123 = await getAllTx(url, address)
-    // let result = {
-    //   ntList: nrList,
-    //   pledgeList: pledgeList
-    // }
-    res.json(response123)
-  })
-
-})
-
- function getAllTx(url, address) {
-
-  if (totalPage === 0) {
-    alert('illegal totalPage')
-  }
-  for (let i = 1; i <= totalPage; i++) {
-    let params = {
-      params: {
-        a: address,
-        p: i
-      }
-    }
-    axios.get(url, params).then(response => {
-      console.log(response.data)
-      let data = response.data
-      totalPage = data.data.totalPage
-      let start = new Date('2019-5-27 12:00:00')
-      let startTimestamp = start.getTime()
-      let end = new Date('2019-6-4 12:00:00')
-      let endTimestamp = end.getTime()
-      let list = data.data.txnList
-      for (let j = 0; j < list.length; j++) {
-        if (list[j].timestamp > startTimestamp && list[j].timestamp < endTimestamp && list[j].status === 1) {
-          let json = JSON.parse(list[j].data)
-          if (json.Function === 'triggerNR') {
-            let item = {
-              hash: list[j].hash,
-              timestamp: list[j].timestamp
-            };
-            nrList.push(item)
-          }
-          if (json.function === 'triggerPledge' || json.Function === 'triggerPledge') {
-            let item = {
-              hash: list[j].hash,
-              timestamp: list[j].timestamp
-            }
-            pledgeList.push(item)
-
-
-          }
+  axios.get(getTxUrl, firstParam).then(response => {
+    let totalPage = response.data.data.totalPage
+    for (let i = 1; i <= totalPage; i++) {
+      let params = {
+        params: {
+          a: pledgeAndNrAddress,
+          p: i
         }
       }
+      axios.get(getTxUrl, params).then(response => {
+        let list = response.data.data.txnList
+        for (let j = 0; j < list.length; j++) {
+          if (list[j].status === 1 && list[j].timestamp > start && list[j].timestamp < end) {
+            let hash = list[j].hash
+            let json = JSON.parse(list[j].data)
+            if (json.function === 'triggerPledge' || json.Function === 'triggerPledge') {
+              let item = {
+                hash: hash
+              }
+              console.log('pledgeTxListItem: ', item)
+              axios.post(getEventByHashUrl,
+                JSON.stringify({hash: hash}),
+                {
+                  headers: {
+                    'Content-Type': 'application/json'
+                  }
+                }).then(response => {
+                let events = response.data.result.events
+                for (let k = 0; k < events.length; k++) {
+                  if (events[k].topic === pledgeAndNrTopic) {
+                    let result = JSON.parse(events[k].data);
+                    let data = result.Produce.data
+                    for (let m = 0; m < data.length; m++) {
+                      pledgeNat += parseFloat(data[m].value)
+                    }
+                  }
+                }
+              })
 
-      let result = {
-        ntList: nrList,
-        pledgeList: pledgeList
-      }
-      return result
+            }
+          }
+        }
+      })
+    }
+  })
+  setTimeout(function () {
+    res.json({
+      pledgeNat: parseInt(pledgeNat / 1000000000000000000),
+      start: start,
+      end: end
     })
+  },10000)
+})
+
+router.get('/nr/nat', function (req, res) {
+  let start = req.query.start
+  let end = req.query.end
+  if (start === undefined || end === undefined || start < 0 || end < 0) {
+    res.json('parameter error!')
   }
-}
+  let nrNat = 0
+  let firstParam = {
+    params: {
+      a: pledgeAndNrAddress,
+      p: 1
+    }
+  }
+  axios.get(getTxUrl, firstParam).then(response => {
+    let totalPage = response.data.data.totalPage
+    for (let i = 1; i <= totalPage; i++) {
+      let params = {
+        params: {
+          a: pledgeAndNrAddress,
+          p: i
+        }
+      }
+      axios.get(getTxUrl, params).then(response => {
+        let list = response.data.data.txnList
+        for (let a = 0; a < list.length; a++) {
+          if (list[a].status === 1 && list[a].timestamp > start && list[a].timestamp < end) {
+            let hash = list[a].hash
+            let json = JSON.parse(list[a].data)
+            if (json.Function === 'triggerNR') {
+              let item = {
+                hash: hash
+              }
+              console.log('nrTxListItem', item)
+              axios.post(getEventByHashUrl,
+                JSON.stringify({hash: hash}),
+                {
+                  headers: {
+                    'Content-Type': 'application/json'
+                  }
+                }).then(response => {
+                let events = response.data.result.events
+                for (let b = 0; b < events.length; b++) {
+                  if (events[b].topic === pledgeAndNrTopic) {
+                    let result = JSON.parse(events[b].data)
+                    let data = result.Produce.data
+                    for (let c = 0; c < data.length; c++) {
+                      nrNat += parseFloat(data[c].value)
+                    }
+                  }
+                }
+              })
+            }
+          }
+        }
+      })
+    }
+  })
+  setTimeout(function () {
+    res.json({
+      nrNat: parseInt(nrNat / 1000000000000000000),
+      start: start,
+      end: end
+    })
+  },12000)
+})
+
+router.get('/vote/nat', function (req, res) {
+  let countList = []
+  let start = req.query.start
+  let end = req.query.end
+  // if (start === undefined || end === undefined || start < 0 || end < 0) {
+  //   res.json('parameter error!')
+  // }
+  let voteRewardNat = 0
+  let firstParam = {
+    params: {
+      a: pledgeAndNrAddress,
+      p: 1
+    }
+  }
+  axios.get(getTxUrl, firstParam).then(response => {
+    let totalPage = response.data.data.totalPage
+    for (let i = 1; i <= totalPage; i++) {
+      let params = {
+        params: {
+          a: voteAddress,
+          p: i
+        }
+      }
+      axios.get(getTxUrl, params).then(response => {
+        let list = response.data.data.txnList
+        console.log('txnList.length: ', list.length)
+        for (let a = 0; a < list.length; a++) {
+          if (list[a].status === 1) {
+            let hash = list[a].hash
+            let json = JSON.parse(list[a].data)
+            if (json.Function === 'vote') {
+              let item = {
+                hash: hash,
+                timestamp: list[a].timestamp
+              }
+              countList.push(item)
+              // axios.post(getEventByHashUrl,
+              //   JSON.stringify({hash: hash}),
+              //   {
+              //     headers: {
+              //       'Content-Type': 'application/json'
+              //     }
+              //   }).then(response => {
+              //   let events = response.data.result.events
+              //   for (let b = 0; b < events.length; b++) {
+              //     if (events[b].topic === voteTopic) {
+              //       let result = JSON.parse(events[b].data)
+              //       voteRewardNat += parseFloat(result.reward)
+              //     }
+              //   }
+              // })
+            }
+          }
+        }
+      })
+    }
+  })
+  setTimeout(function () {
+    res.json({
+      voteRewardNat: parseInt(voteRewardNat / 10),
+      start: start,
+      end: end,
+      length: countList.length
+    })
+  }, 20000)
+})
 
 module.exports = router;
